b3ca040a3173cfe50cbd9ff7bcad35260fa1fd33
Edryel2 <adrielly70@hotmail.com>
2024-04-20 18:11:58 -0300

    Projeto Finalizado

---
+++ medico/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Especialidades, DadosMedicos, DatasAbertas
+
+
+admin.site.register(Especialidades)
+admin.site.register(DadosMedicos)
+admin.site.register(DatasAbertas)
+# Register your models here.
